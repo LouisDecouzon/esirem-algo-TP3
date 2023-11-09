522cfe3ceb8b4fe360bc83881bd0890137112d98
LouisDecouzon <144101139+LouisDecouzon@users.noreply.github.com>
2023-11-09 15:08:44 +0100

    Add files via upload
    suite du tp3 jusqu'a la resolution des equations de degre 2

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 enum SolutionType{
-    None=0,
-    One=1,
-    Two=2
+    NONE=0,
+    ONE=1,
+    TWO=2
 };
 struct Solution{
     enum SolutionType type;
@@ -18,19 +19,55 @@ struct equation{
 };
 void showSolution(struct Solution solution){
     switch(solution.type){
-        case None:
+        case NONE:
             printf("Il n'y a aucune solution.\n");
             break;
-        case One:
-            printf("Il y a une solution : x=%.2f\n", solution.x0);
+        case ONE:
+            printf("Il y a une solution : x=%f\n", solution.x0);
             break;
-        case Two:
-            printf("Il y a 2 solutions : x1=%.2f et x2=%.2f\n",solution.x1,solution.x2);
-            break
+        case TWO:
+            printf("Il y a 2 solutions : x1=%f et x2=%f\n",solution.x1,solution.x2);
+            break;
+    }
+}
+void test() {
+    // Création d'un tableau de Solution avec différents cas
+    struct Solution solutions[] = {
+            {NONE, 0, 0, 0},
+            {ONE, 3.14, 0, 0},
+            {TWO, 2.5, 1.5, 3.0},
+    };
+
+    // Boucle sur le tableau et affichage des résultats
+    for (int i = 0; i < sizeof(solutions)/sizeof(solutions[0]); ++i) { // j'ai cherché comment accéder à la longueur d'un tableau et apparament cette notation avec le quotient est répandue
+        printf("\nTest %d :\n", i + 1);
+        showSolution(solutions[i]);
     }
 }
 
+struct Solution resolveOne(double a, double b){
+    struct Solution sol={ONE,(-b/a),0,0};
+    return sol;
+}
+
+struct Solution resolveTwo(double a,double b, double c){
+    double delta = b*b-4*a*c;
+    if (delta==0){
+        struct Solution sol={ONE,(-b)/(2*a),0,0};
+        return sol;
+    }
+    if (delta<0){
+        struct Solution sol={{NONE},0,0,0};
+        return sol;
+    }
+    if (delta>0){
+        double racine_delta=sqrt(delta);
+        struct Solution sol={TWO,0,(-b-racine_delta)/(2*a),(-b+racine_delta/(2*a))};
+        return sol;
+    }
+}
 int main(){
-    printf("Hello, World!\n");
+    //showSolution(resolveOne(8,2));
+    showSolution(resolveTwo(-2,0,10));
     return 0;
 }
